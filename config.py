613cba9dc787b676b0d0e6227f7cffc7db96ee8b
Muhammadrizo <muhammadrizo1911@gmail.com>
2026-05-09 00:51:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "123456789").split(",")))
+BOT_TOKEN = os.getenv("8655163628:AAEV99aRFnEbhkRAKIwI8IOsOKI4MpOpn4E")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "6917006135").split(",")))
 
 # MySQL sozlamalari
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = int(os.getenv("DB_PORT", "3306"))
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_USER = os.getenv("DB_USER", "ck540806_test")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "test")
 DB_NAME = os.getenv("DB_NAME", "ck540806_test")
